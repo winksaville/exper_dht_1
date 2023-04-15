4c6e5f535fd417eebafae2c8ec9dd2ed41414c80
Wink Saville <wink@saville.com>
2023-04-15 13:41:43 -0700

    feat: Add communication lost DHT ness

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
 use std::collections::HashMap;
-use std::net::SocketAddr;
+use std::net::{TcpListener, TcpStream, Shutdown};
+use std::io::{BufRead, BufReader, Write};
+use std::thread;
 
-type NodeId = u64;
 type Key = String;
 type Value = String;
 
-#[allow(unused)]
+#[derive(Debug)]
 struct Node {
-    id: NodeId,
-    addr: SocketAddr,
+    addr: String,
     storage: HashMap<Key, Value>,
 }
 
 impl Node {
-    fn new(id: NodeId, addr: SocketAddr) -> Self {
+    fn new(addr: &str) -> Self {
         Node {
-            id,
-            addr,
+            addr: addr.to_string(),
             storage: HashMap::new(),
         }
     }
@@ -30,18 +29,71 @@ impl Node {
     }
 }
 
-fn main() {
-    // Create nodes
-    let addr1 = "127.0.0.1:8000".parse().unwrap();
-    let addr2 = "127.0.0.1:8001".parse().unwrap();
-    let mut node1 = Node::new(1, addr1);
-    let mut node2 = Node::new(2, addr2);
-
-    // Store and retrieve data in the DHT
-    node1.store("key1".to_string(), "value1".to_string());
-    node2.store("key2".to_string(), "value2".to_string());
-
-    println!("Node 1: {:?}", node1.retrieve(&"key1".to_string()));
-    println!("Node 2: {:?}", node2.retrieve(&"key2".to_string()));
+enum Action {
+    Continue,
+    Terminate,
 }
 
+fn main() {
+    let mut node1 = Node::new("127.0.0.1:8000");
+    let node2 = Node::new("127.0.0.1:8001");
+
+    node1.store("key1".to_string(), "value1".to_string());
+
+    let node1_listener = TcpListener::bind(&node1.addr).unwrap();
+
+    // Spawn a thread to handle node1's incoming connections
+    let node1_handle = thread::spawn(move || {
+        for stream in node1_listener.incoming() {
+            match stream {
+                Ok(stream) => {
+                    let storage_copy = node1.storage.clone();
+                    let action = handle_client(stream, storage_copy);
+                    if let Action::Terminate = action {
+                        break;
+                    }
+                }
+                Err(e) => {
+                    println!("Error encountered while accepting connection: {}", e);
+                }
+            }
+        }
+    });
+
+    // Connect node2 to node1 and request data
+    let mut stream = TcpStream::connect(&node1.addr).unwrap();
+    writeln!(stream, "GET key1").unwrap();
+
+    let mut reader = BufReader::new(stream);
+    let mut response = String::new();
+    reader.read_line(&mut response).unwrap();
+
+    println!("Node2 received: {}", response.trim());
+
+    // Send termination command
+    let mut stream = TcpStream::connect(&node1.addr).unwrap();
+    writeln!(stream, "TERMINATE").unwrap();
+
+    node1_handle.join().unwrap();
+}
+
+fn handle_client(mut stream: TcpStream, storage: HashMap<Key, Value>) -> Action {
+    let mut reader = BufReader::new(stream.try_clone().unwrap());
+    let mut request = String::new();
+
+    reader.read_line(&mut request).unwrap();
+    if request.trim() == "TERMINATE" {
+        stream.shutdown(Shutdown::Both).unwrap();
+        return Action::Terminate;
+    }
+
+    let key = request.trim().strip_prefix("GET ").unwrap();
+
+    if let Some(value) = storage.get(key) {
+        writeln!(stream, "{}", value).unwrap();
+    } else {
+        writeln!(stream, "Key not found").unwrap();
+    }
+
+    Action::Continue
+}
